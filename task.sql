1d39b4e007d41c226a4a930fd4aa2fc8a569eafa
Greed <daniil.tsitavets@student.ehu.lt>
2024-11-03 14:00:35 +0200

    feature: add solution

--- task.sql
+++ task.sql
@@ -1,1 +1,32 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE ShopDB;
+
+USE ShopDB 
+
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50),
+    Description VARCHAR(50),
+    Price INT,
+    WarehouseAmount INT
+);
+CREATE TABLE Customers (
+	  ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50),
+	  LastName VARCHAR(50),
+    Email VARCHAR(50),
+    Address VARCHAR(50)
+);
+
+CREATE TABLE Orders (
+	  ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT,
+    Date DATE,
+	  FOREIGN KEY (CustomerID) REFERENCES Customers(ID)
+);
+CREATE TABLE OrderItems (
+	  ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ID)
+);
